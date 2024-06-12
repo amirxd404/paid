5194158a48221fefcde6a302f22a45b332c49609
MR. AMIR <150195653+amirxd404@users.noreply.github.com>
2024-06-12 11:42:31 +0500

    Update run.py

--- run.py
+++ run.py
@@ -8,6 +8,6 @@ except:pass
 from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
-  os.system('chmod 777 runs;./runs')
+  os.system('chmod 777 run;./run')
 else:
   exit('Sorry This Tools Not Working 32 Bit Device')
